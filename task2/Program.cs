b52505629869db694334fd4871906d15a839be9f
Dante67yt <dante67yt@gmail.com>
2022-06-26 02:38:18 +0300

    task2 completed

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int x = Convert.ToInt32(0);
+if (a > b) x = a;
+else x = b;
+if (c > x) x = c;
+else Console.Write ("max = ");
+Console.Write (x);
